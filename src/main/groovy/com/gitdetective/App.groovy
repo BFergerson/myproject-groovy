5f8eef177fe71325d4ca8d230cd3c4d76c355a88
Brandon Fergerson <fergerson92@gmail.com>
2018-07-19 11:43:00 -0400

    i'm will smith mixed with micheal cera; i'm micheal cera

---
+++ src/main/groovy/com/gitdetective/App.groovy
@@ -0,0 +1,37 @@
+package com.gitdetective
+
+/**
+ * Hello world!
+ */
+class App {
+
+    static void main(String[] args) {
+        System.out.println("Hello World!")
+    }
+
+    static void internalCall1() {
+        MyClass yay = new MyClass()
+        yay.myMethod()
+    }
+
+    static void internalCall2() {
+        MyClass yay = new MyClass()
+        yay.myMethod2()
+    }
+
+    static void internalCall3() {
+        MyClass yay = new MyClass()
+        yay.myMethod2()
+    }
+
+    static void internalCall4() {
+        MyClass yay = new MyClass()
+        yay.myMethod2()
+    }
+
+    static void internalCall5() {
+        MyClass yay = new MyClass()
+        yay.myMethod2()
+    }
+
+}
---
+++ src/main/groovy/com/gitdetective/MyClass.groovy
@@ -0,0 +1,10 @@
+package com.gitdetective
+
+class MyClass {
+    void myMethod() {
+        System.out.println("Yay :)")
+    }
+    void myMethod2() {
+        System.out.println("Yay :)")
+    }
+}
